afe61d3e8d0e5ac76ef6d1383d1363433c0e1eaa
Aman Ken <13annon@gmail.com>
2018-04-23 18:18:16 +0530

    Final

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ch@+f4&9ttrrfm65@(625(b2f=hs(cql7wmdke!62jdktsxaln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,6 +85,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 """
 DATABASES = {
      'default': {
@@ -92,6 +93,7 @@ DATABASES = {
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
      }
  }
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
